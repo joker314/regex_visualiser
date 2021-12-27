dc5a42d8d40119eb996eaad3cbdf6f8d138bea1f
Joke Book <jokebookyeye@gmail.com>
2021-12-27 17:22:53 +0000

    Make progress on graph renderer

--- graphRenderer.js
+++ graphRenderer.js
@@ -8,5 +8,32 @@ class GraphDrawingEngine {
 		this.canvasElement = canvasElement
 		this.context2d = canvasElement.getContext("2d")
 		this.graphNodes = graphNodes
+		this.scale = 1 // XXX: decided to manually handle coordinates so as to consistently handle mouse movements
+		this.offsetX = 0
+		this.offsetY = 0
+	}
+}
+
+/**
+ * Represents a node
+ */
+class GraphNode {
+	constructor (x, y) {
+		this.x = x
+		this.y = y
+	}
+
+	render (engine) {
+		/**
+		 * Called by the rendering engine so that the node can display itself
+		 */
+	}
+
+	onMouseover () {
+		
+	}
+
+	onMouseout () {
+
 	}
 }
