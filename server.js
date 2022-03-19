e6213527ae9260bab4871295652c7eb87fd056ad
Joke Book <jokebookyeye@gmail.com>
2022-03-19 22:54:06 +0000

    Move files into client directory and create a server directory

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const DEFAULT_PORT = 8000 // port to use if no environment variable set
 const app = express()
 const port = process.env.PORT || DEFAULT_PORT
 
-app.use(express.static('.'))
+app.use(express.static('client'))
 
 app.listen(port, () => {
 	console.log(`Running on port ${port}`)
