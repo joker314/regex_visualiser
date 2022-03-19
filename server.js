0bdf726c9c8216e7c3941fa3da338ccf2ec0c228
Joke Book <jokebookyeye@gmail.com>
2022-03-19 22:48:57 +0000

    Make working server.js file which statically hosts

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const session = require('session')
+const session = require('express-session')
+
+const DEFAULT_PORT = 8000 // port to use if no environment variable set
 
 const app = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || DEFAULT_PORT
 
-app.get('/', (req, res) => {
-	
+app.use(express.static('.'))
+
+app.listen(port, () => {
+	console.log(`Running on port ${port}`)
 })
